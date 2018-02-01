c4d3285220d388fb31cb1ae94683004f6b09ab09
Alex J. Rondón <arondn2@gmail.com>
2018-01-31 23:28:22 -0500

    unit test

--- test/app-base/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const loopback = require('loopback');
-
-const app = module.exports = loopback();
--- test/test.js
+++ test/test.js
@@ -1,10 +1,132 @@
 'use strict';
 
-const assert = require('assert');
-const expect = require('chai').expect;
+const assert     = require('assert');
+const expect     = require('chai').expect;
+const loopback   = require('loopback');
+const supertestp = require("supertest-as-promised")
 
-const app = require('./app-base');
+const api = supertestp('http://localhost:8000');
+
+const LoopbackAllowedPropertiesMixin = require('../');
+
+// -----------------------------------------------------------------------------
+// SETUP LOOPBACK SERVER -------------------------------------------------------
+const app    = module.exports = loopback();
+const ds     = loopback.createDataSource('memory');
+
+const Person = ds.createModel('person', {}, { plural: 'persons' });
+
+Person.remoteMethod("customMethod", {
+  "isStatic": true,
+  "http": {
+    "path": "/customMethod",
+    "verb": "post"
+  },
+  "accepts": [
+    { "arg": "data", "type": "object", "http": { "source": "body" } },
+  ],
+  "returns": { "root": true, "type": "object" }
+});
+Person.customMethod = function (data, cb) {
+  process.nextTick(() => {
+    cb(null, data);
+  });
+};
+
+LoopbackAllowedPropertiesMixin(Person, {
+  create: [
+    'name',
+    'email',
+  ],
+  'prototype.patchAttributes': [
+    'name',
+  ],
+  'customMethod': [
+    'attribute1',
+    'attribute2'
+  ],
+});
+
+const Animal = ds.createModel('animal', {}, { plural: 'animals' });
+LoopbackAllowedPropertiesMixin(Animal);
+
+const Thing = ds.createModel('thing', {}, { plural: 'things' });
+LoopbackAllowedPropertiesMixin(Thing, { create: true });
+
+app.dataSource('ds', ds);
+app.model(Person);
+app.model(Animal);
+app.model(Thing);
+app.use(loopback.rest());
+// SETUP LOOPBACK SERVER END ---------------------------------------------------
+
+
 
 describe('#loopback-allowed-properties-mixin', () => {
+
+  before((done) => {
+    // Listen on HTTP requests
+    app.listen(8000, done);
+  });
+
+  it('existing static method', (done) => {
+    const data = {
+      name: "Alexander Rondon",
+      email: "arondn2@gmail.com",
+      deparment: "Development"
+    };
+    const dataExpected = {
+      name: data.name,
+      email: data.email,
+    };
+    api.post('/persons')
+    .send(data)
+    .expect(function(res) {
+      dataExpected.id = res.body.id;
+    })
+    .expect(200, dataExpected, done);
+  });
+
+  it('existing prototype method', (done) => {
+    const changes = {
+      name: "Alex J. Rondón",
+      email: "arondon@sirideas.com",
+    };
+    const dataExpected = {
+      name: changes.name,
+    }
+    api.get('/persons')
+    .expect(200)
+    .then(function(res) {
+      expect(res.body.length).to.equal(1);
+      return res.body.shift();
+    })
+    .then((data) => {
+      dataExpected.id = data.id;
+      dataExpected.email = data.email;
+      api.patch(`/persons/${data.id}`)
+      .send(changes)
+      .expect(200, dataExpected, done);
+    });
+  });
+
+  it('custom static customMethod', (done) => {
+    const body = {
+      attribute1: "attribute1",
+      attribute2: "attribute2",
+      attribute3: "attribute3",
+    };
+    const bodyExpected = {
+      attribute1: "attribute1",
+      attribute2: "attribute2",
+    };
+    api.post('/persons/customMethod')
+    .send(body)
+    .expect(200, bodyExpected, done);
+  });
+
+  after(() => {
+    process.exit();
+  });
 
 });
